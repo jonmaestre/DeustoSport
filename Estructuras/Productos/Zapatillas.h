e34bc8efd6e68efef3b24daf1d92110e8b1468ae
saaraahdz <sara.hernandez@opendeusto.es>
2022-04-04 12:21:06 +0200

    cambios de estructura

---
+++ Estructuras/Productos/Zapatillas.h
@@ -0,0 +1,15 @@
+#ifndef CALZADO_H_
+#define CALZADO_H_
+
+typedef struct {
+    int id;
+    char nombre;
+    float talla;
+    bool genero;
+    char color;
+    float precio;
+    int cantidad;
+} Calzado;
+
+
+#endif
