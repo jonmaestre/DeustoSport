a3999b5ec177f672e5cef335c3b1156e44838ac5
jonmaestre <jonmaestre@opendeusto.es>
2022-04-09 13:36:56 +0200

    Carrito, Compra y Suplemento

---
+++ Estructuras/Compra.h
@@ -0,0 +1,12 @@
+#ifndef COMPRA_H_
+#define COMPRA_H_
+
+typedef struct {
+   int identificativo;
+   int idProducto;
+   int idComprador;
+   int cantidad;
+} Compra;
+
+
+#endif
---
+++ Estructuras/Suplemento.h
@@ -0,0 +1,11 @@
+#ifndef SUPLEMENTO_H_
+#define SUPLEMENTO_H_
+
+typedef struct {
+   int identificativo;
+   char nombre;
+   char tipo;
+} Suplemento;
+
+
+#endif
