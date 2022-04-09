fdc83e56e9cec8f70853130f6430852de741e5bc
jonmaestre <jonmaestre@opendeusto.es>
2022-04-09 13:31:39 +0200

    Ropa

---
+++ Estructuras/Ropa.h
@@ -0,0 +1,15 @@
+#ifndef ROPA_H_
+#define ROPA_H_
+
+typedef struct {
+    int identificativo;
+    char nombre;
+    char tipo;
+    char color;
+    char talla;
+    float precio;
+    int sexo;
+} Ropa;
+
+
+#endif
